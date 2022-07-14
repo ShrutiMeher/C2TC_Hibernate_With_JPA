f8b3e7aa9fa6ee7c407d96fd308397a18842f7f5
Manohar <Manohar@LAPTOP-ER7UEH8N>
2022-07-14 22:58:23 +0530

    Join

---
+++ JPAJOINInheritance/src/com/training/entity/Inheritance.java
@@ -0,0 +1,43 @@
+package com.training.entity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class Inheritance{
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory= Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em= factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one employee
+		Employee employee=new Employee();
+		employee.setName("Manav");
+		employee.setSalary(10000);
+		em.persist(employee);
+		
+		//create one manager
+		Manager manager=new Manager();
+		manager.setName("Purva");
+		manager.setSalary(12000);
+		manager.setDepartmentName("HR");
+		em.persist(manager);
+		
+		em.getTransaction().commit();
+		
+//				em.close();
+//		factory.close();
+				System.out.println("Added one employee and manager to database.");
+
+	
+	
+	}
+		
+}
+
+	
+	
+
